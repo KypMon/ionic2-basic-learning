a9c4a79aa2c2bea704ff563640a8362e47520cb3
KanesYang <pyang64@uwo.ca>
2017-05-10 22:55:27 -0400

    basic view set up

--- recipe-app/src/pages/tabs/tabs.ts
+++ recipe-app/src/pages/tabs/tabs.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Tabs page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { ShoppingListPage } from './../shopping-list/shopping-list';
+import { RecipesPage } from './../recipes/recipes';
+
 @IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
+
+  slPage = ShoppingListPage;
+  recipesPage = RecipesPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
