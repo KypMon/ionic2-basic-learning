e59b88ee937930e4b1feeedb3e7980c4d78e4135
KanesYang <978634806@qq.com>
2017-05-14 19:03:58 -0400

    basic set up of myworkout app

---
+++ myworkouts/src/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { WorkoutsPage } from './../workouts/workouts';
+import { AddWorkoutPage } from './../add-workout/add-workout';
+
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = WorkoutsPage;
+  tab2Root = AboutPage;
+  tab3Root = AddWorkoutPage;
+  
+
+  constructor() {
+
+  }
+}
